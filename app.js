96bb7930915317e4aba8e44815f1a13a592b22f7
amirsaranBIH <amir.saranbih@gmail.com>
2018-03-13 17:46:40 +0100

    Added routes, authentication and more file structure

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
 var express = require('express'),
-    bodyParser = require('body-parser');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+    expressSession = require('express-session'),
+    User = require('./models/User');
+
+mongoose.connect('mongodb://localhost/sharecode');
 
 var app = express();
 
+// Passport config
+app.use(expressSession({
+  secret: 'This is the secret sentence',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// App config
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 // Routes
 app.use(require('./routes/index'));
+app.use(require('./routes/auth'));
 
-app.listen(3000, () => {
+var port = process.env.PORT || 3000;
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB server up and running...');
+}).on('error', (err) => {
+  console.log('Error: ' + err);
+});
+
+app.listen(port, () => {
   console.log('Server started and running on port 3000...');
 });
---
+++ routes/auth.js
@@ -0,0 +1,43 @@
+var router = require('express').Router(),
+    passport = require('passport'),
+    User = require('../models/User');
+
+// Login routes
+router.route('/login')
+    .get((req, res) => {
+      res.render('login');
+    })
+    .post(passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/login'
+    }));
+
+// Sign up routes
+router.route('/signup')
+    .get((req, res) => {
+      res.render('signup');
+    })
+    .post((req, res) => {
+      var newUser = {
+        username: req.body.username,
+        email: req.body.email
+      };
+      User.register(newUser, req.body.password, (err, user) => {
+        if (err) {
+          console.log('Error: ' + err.toString());
+          return res.redirect('/signup');
+        }
+        passport.authenticate('local')((req, res) => {
+          res.redirect('/');
+        });
+      });
+    });
+
+// Logout route
+router.route('/logout')
+    .get((req, res) => {
+      req.logout();
+      req.redirect('/');
+    });
+
+module.exports = router;
