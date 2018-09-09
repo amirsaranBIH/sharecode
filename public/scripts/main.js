8172d383726dd4f56cafd057e9d9dcf7822f7fb2
amirsaranBIH <amir.saranbih@gmail.com>
2018-09-09 15:39:02 +0200

    Revert "Home page HTML and CSS"
    This reverts commit 097fc81f443bf1466027153fc9897945d430d65a.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,14 +6,3 @@ if (document.getElementById('exit-flash-box')) {
     exitFlashBoxButton.parentNode.style.display = 'none';
   });
 }
-
-$('#navicon').click(() => {
-  if (document.getElementById('navicon').style.top !== '142px') {
-    document.getElementById('navicon').style.top = '142px';
-    document.getElementsByClassName('navicon-lines')[0].style.transform = 'rotateZ(-45deg)';
-  } else {
-    document.getElementById('navicon').style.top = '-30px';
-    document.getElementsByClassName('navicon-lines')[0].style.transform = 'rotateZ(-45deg) translateY(15px)';
-  }
-  $('.header-navigation ul').slideToggle(600);
-});
